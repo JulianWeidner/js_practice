53f28b566955c98f40a61ecb0f4a3ad7c155e011
Julian Weidner <38364739+JulianWeidner@users.noreply.github.com>
2018-10-03 19:36:41 -0500

    Worked on palindrome
    Check into turning the array into a char array then reversing.

--- javascript-exercises/palindromes/palindromes.js
+++ javascript-exercises/palindromes/palindromes.js
@@ -2,4 +2,32 @@ const palindromes = function() {
 
 }
 
+
+function Palindrome(stringParam){
+//create array of WORDS
+	let stringArray = stringParam.split(" ");
+  let reverseArray = stringArray; 
+  let straightString = stringArray.join('');
+  reverseArray.reverse(); 
+  let reverseString = reverseArray.join(''); 
+  
+  reverseString.toLowerCase();
+  straightString.toLowerCase();
+  
+  if(reverseString === straightString){
+  	console.log(straightString + " === " + reverseString);
+    console.log("Palindrome")
+  	return true;
+  }
+  else{
+  	console.log(straightString + " != " + reverseString);
+  	console.log("Not a Palindrome");
+  	return false;
+  }
+  
+  
+  
+
+}
+
 module.exports = palindromes
